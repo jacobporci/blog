26393daed5d31062666199e664d73dc9feedc33b
Jacob Porciuncula <jacobporci@gmail.com>
2023-04-24 21:54:53 +0800

    create card components

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,47 @@
+import { ShowResponse } from "@/pages/api/netflix/show/[id]";
+import { api } from "@/utils/api";
+import { AxiosResponse } from "axios";
+import Image from "next/image";
+import { useQuery } from "react-query";
+
+interface Props {
+  id: string;
+}
+
+const fetchCard = async ({ queryKey }: { queryKey: string[] }) => {
+  const [_, id] = queryKey;
+  try {
+    const { data }: AxiosResponse<ShowResponse> = await api.get(
+      `/netflix/show/${id}`
+    );
+    return data.data;
+  } catch (error) {
+    console.log("error", error);
+    throw new Error("Something went wrong");
+  }
+};
+export default function Card({ id }: Props) {
+  const { data, isLoading, error } = useQuery(["card", id], fetchCard);
+
+  if (isLoading || error) {
+    return (
+      <div className="text-blue-800 h-80 w-52 flex items-center justify-center border-2 rounded-xl">
+        ...loading...
+      </div>
+    );
+  }
+  return data ? (
+    <div className="text-blue-800 rounded-xl">
+      {/* <h1>{data?.name}</h1> */}
+      <Image
+        className="rounded-xl"
+        src={`${data?.imageSrc}?random=${data?.id}`}
+        height={300}
+        width={200}
+        alt={data?.name}
+      />
+    </div>
+  ) : (
+    <>heloo</>
+  );
+}
---
+++ src/components/CardList.tsx
@@ -0,0 +1,18 @@
+import { Genre } from "@/pages/api/netflix/genres";
+import Card from "./Card";
+
+interface Props {
+  data: Array<Genre>;
+}
+export default function CardList({ data }: Props) {
+  return data.map(({ name, shows }) => (
+    <div key={name}>
+      <h1 className="text-black text-xl font-bold">{name}</h1>
+      <div className="flex gap-5">
+        {shows.map((id) => (
+          <Card key={id} id={id} />
+        ))}
+      </div>
+    </div>
+  ));
+}
